04c3073bfb6d0b3fe01b42f0b4720bdcc8802417
txdr <radltaylor189@gmail.com>
2024-09-08 17:45:14 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require("http");
+const express = require("express");
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.static("views"));
+server.listen(8080);
+
+console.log("Server online, accessible at http://localhost:8080/");
